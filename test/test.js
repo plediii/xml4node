a8f159083ea6dfd9f4d021383d2a877acd6c6924
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-06-26 14:55:24 +0000

    Add processing instruction tests.

--- test/test.js
+++ test/test.js
@@ -23,9 +23,35 @@ describe('parseString', function () {
         });
     });
 
+    it('should correctly parse an xml prolog', function () {
+        var doc = xml.parseString('<?xml version="6.0"?><server />');
+        assert(doc.root);
+        assert(doc.prolog);
+        assert.equal(doc.root.name, 'server');
+        assert.equal(doc.prolog.body, 'version="6.0"');
+    });
+
+
+    it('should correctly parse an xml prolog even in the presence of processing instructions', function () {
+        var doc = xml.parseString('<?xml version="6.0"?><server><?xml version="7.1"?></server>');
+        assert(doc.root);
+        assert(doc.prolog);
+        assert.equal(doc.root.name, 'server');
+        assert.equal(doc.root.children.length, 1);
+        assert.equal(doc.prolog.body, 'version="6.0"');
+    });
+
+
 });
 
 describe('parseNode', function () {
+
+    it('should correctly parse a processing instruction', function () {
+        var node = xml.parseNode('<?xml version="8.6"?>');
+        assert.equal(node.type, 'processing')
+        assert.equal(node.body, 'version="8.6"');
+    });
+
 
     it('should correctly parse a root node', function () {
         var node = xml.parseNode('<server />');
@@ -197,9 +223,6 @@ describe('nodeToString', function () {
         var elt = xml.elt('character', [xml.text('&')]);
         assert.equal(xml.nodeToString(elt), '<character>&amp;</character>');
     });
-
-
-
 
 });
 
