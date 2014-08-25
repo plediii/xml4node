bf9af1b588bbb3e0a47582c5ba7513372d901078
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-08-25 15:49:11 +0000

    Explicitly set pretty to false in non-pretty print tests.

--- test/nodeToString.js
+++ test/nodeToString.js
@@ -13,94 +13,94 @@ describe('nodeToString', function () {
 
         it('should correctly output an empty node', function () {
             var elt = xml.elt('alien');
-            assert.equal(xml.nodeToString(elt), '<alien/>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<alien/>');
         });
 
         it('should correctly convert an element with a child to a string', function () {
             var elt = xml.elt('tag', [xml.elt('child')]);
-            assert.equal(xml.nodeToString(elt), '<tag><child/></tag>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<tag><child/></tag>');
             testParents(elt);
         });
 
         it('should correctly ignore isSelfClosing attribute', function () {
             var elt = xml.elt('tag', [xml.elt('child')]);
             elt.isSelfClosing = true;
-            assert.equal(xml.nodeToString(elt), '<tag><child/></tag>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<tag><child/></tag>');
             testParents(elt);
         });
 
         it('should correctly output text nodes', function () {
             var elt = xml.elt('tag', [xml.text('child')]);
-            assert.equal(xml.nodeToString(elt), '<tag>child</tag>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<tag>child</tag>');
             testParents(elt);
         });
 
         it('should correctly output an attribute', function () {
             var elt = xml.elt('alien', { type: 'extra'});
-            assert.equal(xml.nodeToString(elt), '<alien type="extra"/>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<alien type="extra"/>');
             testParents(elt);
         });
 
         it('should correctly escape an attribute with a quote', function () {
             var elt = xml.elt('character', { is: '"'});
-            assert.equal(xml.nodeToString(elt), '<character is="&quot;"/>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<character is="&quot;"/>');
             testParents(elt);
         });
 
         it('should correctly escape an attribute with a <', function () {
             var elt = xml.elt('character', { is: '<'});
-            assert.equal(xml.nodeToString(elt), '<character is="&lt;"/>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<character is="&lt;"/>');
             testParents(elt);
         });
 
 
         it('should correctly escape an attribute with a >', function () {
             var elt = xml.elt('character', { is: '>'});
-            assert.equal(xml.nodeToString(elt), '<character is="&gt;"/>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<character is="&gt;"/>');
             testParents(elt);
         });
 
         it('should correctly escape an attribute with a &', function () {
             var elt = xml.elt('character', { is: '&'});
-            assert.equal(xml.nodeToString(elt), '<character is="&amp;"/>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<character is="&amp;"/>');
             testParents(elt);
         });
 
 
         it('should correctly output an attribute and child node', function () {
             var elt = xml.elt('alien', { type: 'person'}, [xml.elt('sigourney')]);
-            assert.equal(xml.nodeToString(elt), '<alien type="person"><sigourney/></alien>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<alien type="person"><sigourney/></alien>');
             testParents(elt);
         });
 
         it('should correctly escape text a quote', function () {
             var elt = xml.elt('character', [xml.text('"')]);
-            assert.equal(xml.nodeToString(elt), '<character>&quot;</character>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<character>&quot;</character>');
             testParents(elt);
         });
 
         it('should correctly escape text with a <', function () {
             var elt = xml.elt('character', [xml.text('<')]);
-            assert.equal(xml.nodeToString(elt), '<character>&lt;</character>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<character>&lt;</character>');
             testParents(elt);
         });
 
 
         it('should correctly escape text with a >', function () {
             var elt = xml.elt('character', [xml.text('>')]);
-            assert.equal(xml.nodeToString(elt), '<character>&gt;</character>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<character>&gt;</character>');
             testParents(elt);
         });
 
         it('should correctly escape text with a &', function () {
             var elt = xml.elt('character', [xml.text('&')]);
-            assert.equal(xml.nodeToString(elt), '<character>&amp;</character>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<character>&amp;</character>');
             testParents(elt);
         });
 
         it('should correctly output a prolog processing instruction', function () {
             var elt = xml.elt('photoshoot', [xml.processinginstruction('super', 'model')]);
-            assert.equal(xml.nodeToString(elt), '<photoshoot><?super model?></photoshoot>');
+            assert.equal(xml.nodeToString(elt, { pretty: false }), '<photoshoot><?super model?></photoshoot>');
             testParents(elt);
         });
 
